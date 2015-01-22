b7d441dd4674ff78f3b9d2db15f244fd8baaa5fb
anderswallenquist <anders.wallenquist@vertel.se>
2015-01-22 12:08:02 +0100

    Initial commit

---
+++ website_ad/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Website Adverticement',
+    'category': 'Website',
+    'summary': 'Manage online adverticements',
+    'version': '1.1',
+    'description': """
+Website Adverticements
+===========
+
+
+        """,
+    'author': 'Vertel AB',
+    'depends': ['website',],
+    'data': [
+        'views/templates.xml',
+    ],
+    'demo': [ ],
+    'installable': True,
+    'application': True,
+    'sequence': 80,
+}
